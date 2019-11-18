3008673dc1e67f9769109d4d05a963dd51940d55
AntiAegis <thuypn9a4@gmail.com>
2019-11-18 13:47:12 +0700

    update

--- ccdetection/configs/retina_mask/retinamask_r50_fpn_1x.py
+++ ccdetection/configs/retina_mask/retinamask_r50_fpn_1x.py
@@ -121,7 +121,7 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    imgs_per_gpu=8,
+    imgs_per_gpu=16,
     workers_per_gpu=8,
     train=dict(
         type=dataset_type,
@@ -139,7 +139,7 @@ data = dict(
         img_prefix=data_root + 'images/val2017/',
         pipeline=test_pipeline))
 # optimizer
-optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.03, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
 lr_config = dict(
