0ebeea071299fca00585f7245ebccd35ade17f8e
AntiAegis <thuypn9a4@gmail.com>
2019-11-19 11:10:36 +0700

    update

--- command_train.sh
+++ command_train.sh
@@ -1,11 +1,8 @@
 CONFIG_FILE='ccdetection/configs/retina_mask/retinamask_r50_fpn_1x.py'
-GPUS=4
+GPUS=1
 SEED=0
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=0
 python -m torch.distributed.launch --nproc_per_node=$GPUS \
 	--master_port=$((RANDOM + 10000)) \
     mmdetection/tools/train.py $CONFIG_FILE --launcher pytorch --seed $SEED  #--validate
-
-# export CUDA_VISIBLE_DEVICES=1
-# python  mmdetection/tools/train.py $CONFIG_FILE
