9d7a4b38cdd2e475cee4c0a5704565c3cc5bf71e
Anh Vo Tran Hai <vnbot2@gmail.com>
2019-11-19 10:05:10 +0000

    ok

--- install.sh
+++ install.sh
@@ -1,12 +1,4 @@
-rm -rf mmdetection
-git clone https://github.com/open-mmlab/mmdetection.git
-cd mmdetection
-git reset --hard 4d84161f142b7500089b0db001962bbc07aa869d
-pip install -v -e .
-
-# Create symbolic link from ccdetection to mmdetection
-cd ..
-python ccdet_setup.py
+pip install -e ./
 conda install mpmath pandas -y
 conda install -c conda-forge json_tricks -y
 pip install torch_dct imagecorruptions albumentations pycocotools
