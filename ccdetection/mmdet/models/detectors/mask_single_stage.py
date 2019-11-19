5d97d1c5edcc426e80bf6d8bf7ffba08c71b8303
AntiAegis <thuypn9a4@gmail.com>
2019-11-19 10:35:42 +0700

    update

--- ccdetection/mmdet/models/detectors/mask_single_stage.py
+++ ccdetection/mmdet/models/detectors/mask_single_stage.py
@@ -113,18 +113,13 @@ class MaskSingleStateDetector(BaseDetector, BBoxTestMixin, MaskTestMixin):
 		return losses
 
 	def simple_test(self, img, img_meta, proposals=None, rescale=False):
-		"""Test without augmentation."""
-		assert self.with_bbox, "Bbox head must be implemented."
-
 		# BBox
 		x = self.extract_feat(img)
 		outs = self.bbox_head(x)
 		bbox_inputs = outs + (img_meta, self.test_cfg, rescale)
 		bbox_list = self.bbox_head.get_bboxes(*bbox_inputs)
-		bbox_results = [
-			bbox2result(det_bboxes, det_labels, self.bbox_head.num_classes)
-			for det_bboxes, det_labels in bbox_list
-		]
+		det_bboxes, det_labels = tuple(zip(*bbox_list))
+		bbox_results = bbox2result(det_bboxes, det_labels, self.bbox_head.num_classes)
 
 		# Mask
 		if not self.with_mask:
