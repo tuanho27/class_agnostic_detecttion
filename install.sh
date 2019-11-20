7a92623d1e409670cb600038501af9a52fd5b652
Anh Vo Tran Hai <vnbot2@gmail.com>
2019-11-20 03:31:43 +0000

    ok

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@ pip install -e ./
 conda install mpmath pandas -y
 conda install -c conda-forge json_tricks -y
 pip install torch_dct imagecorruptions albumentations pycocotools
-pip install -e mmdetection/mmdet/models/backbones/pytorch-image-models
+pip install -e mmdet/models/backbones/pytorch-image-models
 pip install git+https://github.com/vnbot2/pyson
