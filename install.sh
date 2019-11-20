5ddda81bc3c7f7cddd223993d3d776e9157ddbe9
Anh Vo Tran Hai <vnbot2@gmail.com>
2019-11-20 03:35:54 +0000

    checkout pytorch-iamge=model from master

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ conda install -c conda-forge json_tricks -y
 pip install torch_dct imagecorruptions albumentations pycocotools
 pip install -e mmdet/models/backbones/pytorch-image-models
 pip install git+https://github.com/vnbot2/pyson
+
+git mv ccdetection/mmdet/models/backbones/pytorch-image-models ./mmdet/models/backbones/pytorch-image-models
