450eea7825e063f6e8cc794375aeef31d1e9ef34
AntiAegis <thuypn9a4@gmail.com>
2019-11-20 10:38:28 +0700

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,2 @@
 pip install -e ./
-conda install mpmath pandas -y
-conda install -c conda-forge json_tricks -y
-pip install torch_dct imagecorruptions albumentations pycocotools
 pip install -e mmdet/models/backbones/pytorch-image-models
-pip install git+https://github.com/vnbot2/pyson
