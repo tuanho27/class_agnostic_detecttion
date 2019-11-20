3994e455e8a2d66aeb4cadaca115743a17565806
Anh Vo Tran Hai <vnbot2@gmail.com>
2019-11-20 04:43:17 +0000

    ok

--- configs/fp16/retinamask_b1_fpn_1x.py
+++ configs/fp16/retinamask_b1_fpn_1x.py
@@ -158,6 +158,7 @@ data = dict(
         pipeline=test_pipeline, num_samples=num_samples))
 # optimizer
 optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
+# optimizer = dict(type='Adam', lr=0.002)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
 lr_config = dict(
