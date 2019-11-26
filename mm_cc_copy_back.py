cffbb8bd4200eb5cdcd7b4203e7781e2320b73fa
Anh Vo Tran Hai <vnbot2@gmail.com>
2019-11-26 03:41:00 +0000

    ok

--- mm_cc_copy_back.py
+++ mm_cc_copy_back.py
@@ -2,8 +2,13 @@ from glob import glob
 import os
 os.remove('_mm_status.txt')
 os.system('cd mmdetection && echo $(git status) >> ../_mm_status.txt')
-os.system(
-    'cp -r mmdetection origin_mmdetection && cd origin_mmdetection && git checkout -- .')
+
+assert os.path.exists('origin_mmdetection')
+
+# os.system(
+#     'cp -r mmdetection origin_mmdetection && cd origin_mmdetection && git checkout -- .')
+
+
 #Step1 Copy new, modified files
 def find_all_file(text):
     possible_paths = text.split(' ')
@@ -22,7 +27,7 @@ for path in paths:
     is_file = os.path.isfile(f'mmdetection/{path}')
 
     if not_symlink and (is_none_existed_dir or is_file):
-        print(f'cp -r mmdetection/{path} ccdetection/{path} ')
+        print(f'copy-to-cc: {path} ')
         os.system(f'cp -r mmdetection/{path} ccdetection/{path} ')
 
 ##Step2 remove duplicate file with origin mm
@@ -45,6 +50,7 @@ for lvl in range(6):
 for cc_file in cc_files:
     origin_mmfile = cc_file.replace('ccdetection/', 'origin_mmdetection/')
     if is_same_content(cc_file, origin_mmfile):
+        os.remove(cc_file)
         print('remove for being duplicate with origin mmdetection:', cc_file)
 
 os.system('rm -rf origin_mmdetection')
